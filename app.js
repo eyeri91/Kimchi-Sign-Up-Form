941dbe8b66e0e96b450392ea8c04b3a0063b25a2
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-07-03 20:53:31 +0400

    Check input id and verify the length of value accordingly

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const pass1 = document.getElementById('password1');
 const pass2 = document.getElementById('password2');
+const inputs = document.querySelector('input');
+
 
 jumpToJoin.addEventListener('click', () => {
     window.scrollTo(0, document.body.scrollHeight);
@@ -20,9 +22,18 @@ lname.addEventListener('change', (e) => {
 
 function checkInput(e) {
     let info = e.target.value;
-    if (info.length < 3) {
-        e.target.style.border = "red solid 1px";
-    } else if (info.length >= 3) {
-        e.target.style.border = "green solid 1px";
+    // Check input id and check the value length accordingly.
+    if (e.target.id == 'fname') {
+        if (info.length < 2) {
+            e.target.style.border = "red solid 1px";
+        } else if (info.length >= 2) {
+            e.target.style.border = "green solid 1px";
+        }
+    } else if (e.target.id == 'lname') {
+        if (info.length < 3) {
+            e.target.style.border = "red solid 1px";
+        } else if (info.length >= 3) {
+            e.target.style.border = "green solid 1px";
+        }
     }
 }
